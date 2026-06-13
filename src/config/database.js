50378aad1b1eaadfbf67c088ca1d26c553e62236
Himanshu Gola <himanshu.gola05@gmail.com>
2026-06-13 15:38:14 +0530

    db

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 
 async function connectToDB() {
 
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
     try {
         await mongoose.connect(process.env.MONGO_URI)
 
